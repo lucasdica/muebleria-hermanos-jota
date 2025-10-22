44ba6ba23d0c0e3ec9208a90bf09b39de59b9d80
leandro-quiroga <leandroquiroga280@gmail.com>
2025-10-22 00:45:17 -0300

    mejora de logger, agrego .gitignore

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,6 +1,18 @@
 export function logger(req, res, next) {
-    
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`)
+    const now = new Date();
 
+    const argentinaTime = now.toLocaleString('es-AR', {
+        timeZone: 'America/Argentina/Buenos_Aires',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+    
+    console.log(`[${argentinaTime}] ${req.method} ${req.originalUrl}`);
+    
     next();
 }
