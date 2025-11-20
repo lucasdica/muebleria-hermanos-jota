e569640bf10588c509cbb21f7b5e1540ef3ee393
leandro-quiroga <leandroquiroga280@gmail.com>
2025-11-20 19:43:21 -0300

    agrego endpoint /perfil, middleware autenticar

--- backend/rutas/ruta.usuario.js
+++ backend/rutas/ruta.usuario.js
@@ -1,82 +1,101 @@
-import {Router} from 'express'
-import {Usuario} from '../modelos/modelo.usuario.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import { Router } from "express";
+import { Usuario } from "../modelos/modelo.usuario.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { autenticarUsuario } from "../middlewares/autenticacion.js";
 
 const rutasUsuario = Router();
 
-rutasUsuario.post('/registro', async (req, res) =>{
-    try {
-        
-        const { nombre, apellido, email, fechaNacimiento, clave, fotoPerfil } = req.body;
+rutasUsuario.post("/registro", async (req, res) => {
+  try {
+    const { nombre, apellido, email, fechaNacimiento, clave, fotoPerfil } =
+      req.body;
 
-        const existeUsuario = await Usuario.findOne({ $or: [{email}]});
+    const existeUsuario = await Usuario.findOne({ $or: [{ email }] });
 
-        if(existeUsuario) {
-            return res.status(400).json({mensaje: 'El correo ya esta registrado'});
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(clave, salt);
-
-        //paso 4
-        const nuevoUsuario = new Usuario({
-            nombre: nombre.trim(),
-            apellido: apellido.trim(),
-            email: email.toLowerCase().trim(),
-            clave: hash,
-            fechaNacimiento: new Date(fechaNacimiento), //fechaNacimiento: Date.parse(fechaNacimiento) Number
-            fotoPerfil: fotoPerfil || null,
-            misPedidos: []
-        });
-
-        const usuarioGuardado = await nuevoUsuario.save();
-
-        console.log(`Usuario creado con exito, _id: ${usuarioGuardado._id} `);
-
-        const usuarioRespuesta = usuarioGuardado.toObject();
-        delete usuarioRespuesta.clave;
-
-        return res.status(201).json({mensaje: 'Usuario creado con exito', usuario: usuarioRespuesta});
-
-    } catch (error) {
-        res.status(500).json({mensaje: 'Error interno del servidor'});
+    if (existeUsuario) {
+      return res.status(400).json({ mensaje: "El correo ya esta registrado" });
     }
-})
 
-rutasUsuario.post('/login', async (req, res) => {
-try {
-    
-    const usuario = await Usuario.findOne({email: req.body.email});
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(clave, salt);
 
-    if(!usuario) {
-        return res.status(400).json({mensaje: 'Credenciales invalidas'});
+    //paso 4
+    const nuevoUsuario = new Usuario({
+      nombre: nombre.trim(),
+      apellido: apellido.trim(),
+      email: email.toLowerCase().trim(),
+      clave: hash,
+      fechaNacimiento: new Date(fechaNacimiento), //fechaNacimiento: Date.parse(fechaNacimiento) Number
+      fotoPerfil: fotoPerfil || null,
+      misPedidos: [],
+    });
+
+    const usuarioGuardado = await nuevoUsuario.save();
+
+    console.log(`Usuario creado con exito, _id: ${usuarioGuardado._id} `);
+
+    const usuarioRespuesta = usuarioGuardado.toObject();
+    delete usuarioRespuesta.clave;
+
+    return res
+      .status(201)
+      .json({ mensaje: "Usuario creado con exito", usuario: usuarioRespuesta });
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error interno del servidor" });
+  }
+});
+
+rutasUsuario.post("/login", async (req, res) => {
+  try {
+    const usuario = await Usuario.findOne({ email: req.body.email });
+
+    if (!usuario) {
+      return res.status(400).json({ mensaje: "Credenciales invalidas" });
     }
 
     const claveValida = await bcrypt.compare(req.body.clave, usuario.clave);
 
-    if(!claveValida){
-        return res.status(400).json({mensaje: 'Credenciales invalidas'});
+    if (!claveValida) {
+      return res.status(400).json({ mensaje: "Credenciales invalidas" });
     }
 
     const jwtUsuario = jwt.sign(
-        {id: usuario._id, email: usuario.email},
-        process.env.SECRETO_JWT,
-        {expiresIn: '1h'}
+      { id: usuario._id, email: usuario.email },
+      process.env.SECRETO_JWT,
+      { expiresIn: "1h" }
     );
 
     res.status(200).json({
-        jwtUsuario,
-        usuario: {
-            id: usuario._id,
-            email: usuario.email
-        }
+      jwtUsuario,
+      usuario: {
+        id: usuario._id,
+        email: usuario.email,
+      },
     });
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error interno del servidor" });
+  }
+});
 
-} catch (error) {
-    res.status(500).json({mensaje: 'Error interno del servidor'})
-}
-})
+rutasUsuario.get("/perfil", autenticarUsuario, async (req, res) => {
+  try {
+    const usuarioId = req.user.id;
 
+    const usuarioInfo = await Usuario.findById(usuarioId);
+
+    if (!usuarioInfo) {
+      res.status(404).json({mensaje: "Error al intentar obtener la informacion de perfil",});
+    }
+
+    res.status(200).json(usuarioInfo);
+
+  } catch (error) {
+    console.error(`Error al buscar el Usuario`, error.message);
+    error.status = 500;
+    next(error);
+  }
+});
 
 export default rutasUsuario;
